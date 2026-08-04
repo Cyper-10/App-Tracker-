a7947dbf569d9c9bd7c0d8666cac6c46f6e360aa
P4trickM4rc <112623691+Cyper-10@users.noreply.github.com>
2026-08-04 12:50:40 +0800

    c=push

---
+++ app/map.js
@@ -0,0 +1,27 @@
+'use client';
+
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+// Fix default Leaflet marker icon issue in Next.js
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+});
+
+export default function Map() {
+  return (
+    <MapContainer center={[10.7202, 122.5621]} zoom={13} style={{ height: '100%', width: '100%' }}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[10.7202, 122.5621]}>
+        <Popup>App Tracker Map Active!</Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
