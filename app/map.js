b4ddcc4996f8c8beded0d6fc795aaaf72fb47943
Cyper-10 <marcllacer01@gmail.com>
2026-09-12 14:02:19 +0800

    updated API
    API need for for location

--- app/map.js
+++ app/map.js
@@ -434,7 +434,7 @@ export default function Map() {
         >
           <TileLayer
             attribution='&copy; CARTO'
-            url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+            url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png?key=cb1_3i1g_1_84bc385403b805786774790e"
             noWrap={true}
             bounds={outerWorldBounds}
           />
